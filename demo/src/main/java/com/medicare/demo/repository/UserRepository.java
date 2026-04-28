15e6370db3ac24ed548d6b8877b11b841e1853f7
sri ram <sri812469@gmail.com>
2026-04-28 12:31:50 +0530

    sri

---
+++ demo/src/main/java/com/medicare/demo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.medicare.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.medicare.demo.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
