41971b51392ec074bc1fdac431d4f42e8e13f2d0
Maharaj P S <maharajpsd@gmail.com>
2026-04-28 13:03:14 +0530

    Added cart and order entity

---
+++ demo/src/main/java/com/medicare/demo/entity/CartItem.java
@@ -0,0 +1,33 @@
+package com.medicare.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @com.fasterxml.jackson.annotation.JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medicine_id", nullable = false)
+    private Medicine medicine;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dosage_id", nullable = false)
+    private Dosage dosage;
+
+    @Column(nullable = false)
+    private Integer quantity;
+}
